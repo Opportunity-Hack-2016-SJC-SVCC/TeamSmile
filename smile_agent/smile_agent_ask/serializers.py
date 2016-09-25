90f5a677acb9c093d7fc7690a570d90c5924b7b6
kosmori <kosmori@cisco.com>
2016-09-25 01:53:18 -0700

    Implement without telglam and time

--- smile_agent/smile_agent_ask/serializers.py
+++ smile_agent/smile_agent_ask/serializers.py
@@ -9,11 +9,36 @@ class InterpreterSerializer(serializers.Serializer):
         model = "hoge"
         fields = ('id', 'text')
 
+    def send_reply(self, client_type, client_id, message):
+        # Using Telegram API?, send result to a client.
+        print(message)
+        if client_type.lower() == 'telegram':
+            print('telegram')
+            # TODO
+            # send_request = 'http://139.59.212.15:3045/api/uid/%s/%s' % (client_data[0], place_request_message)
+        elif client_type.lower() == 'sms':
+            print('sms')
+            send_request = 'http://139.59.210.181:8081/sms'
+            requests.post(send_request, data={'number': client_id, 'message': message})
+            # send_request = 'http://139.59.212.15:3045/api/uid/%s/%s' % (client_data[0], place_request_message)
+
     def create(self, validated_data):
         print(validated_data)
-        # validated_data = {'locations':['sanjose']}
-        locations = validated_data['locations']
+        validated_data = {'protocol':'SMS', 'number':'num1', 'time':'time1', 'service':'sv1', 'subservice':'subsv1', 'locations':['sanjose']}
+        if not validated_data:
+            print('Error: POST-data is empty')
+            return True
+        client_type = validated_data.get('protocol')
+        client_id = validated_data.get('number')
+        time = validated_data.get('time')
+        service = validated_data.get('service')
+        subservice = validated_data.get('subservice')
+        locations = validated_data.get('locations')
         print(locations)
+        if not (client_type and client_id and time and service and subservice):
+            print('Error: POST-data-value is empty')
+            return True
+
         if locations:  # Send client about the near free food place
             print('OK')
             for location in locations:
@@ -29,20 +54,20 @@ class InterpreterSerializer(serializers.Serializer):
                     db_request = 'http://139.59.212.15:3045/api/food_source/?latitude=%s&longitude=%s' % (lat, lng)
                     print(db_request)
                     db_result = requests.get(db_request)
-                    print(db_result.content)
+                    res = db_result.content.decode("utf-8")
+                    json_res = json.loads(res)
+                    r = json_res['0']
+                    self.send_reply(client_type, client_id, ("Hello! The nearest free food is in %s miles(?). "
+                                                             "There is %s. Open a further %s hours." % (r['distance'], r['address'], "TIME")))
                 else:
                     print('OK-NG')
-                    # Using Telegram API, send result to a client(Please send place
+                    self.send_reply(client_type, client_id, "[%s] is ambiguous. Please send again with more clear address." % location)
 
-
-            # Using Telegram API, send result to a client.
         else:  # No location info! => Ask client again
             print('NG')
-            # Using Telegram API, send result to a client.
+            self.send_reply(client_type, client_id, "Please send message with address.")
 
-        ret = 'mokemoke'
-        # ret = self.kwargs.get('location')
-        return ret  # No meaning
+        return True  # No meaning
 
 
 
