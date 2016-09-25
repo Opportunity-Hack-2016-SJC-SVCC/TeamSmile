acdf03943bcf8dbab26ef7589d2e30abe1043c97
Matvey <motakuk@gmail.com>
2016-09-25 00:58:53 -0700

    User's distance

--- core_db/src/smile_api/models.py
+++ core_db/src/smile_api/models.py
@@ -1,18 +1,39 @@
 from django.contrib.gis.db import models
-from django.contrib.gis.geos import GEOSGeometry
-from django.contrib.gis.measure import Distance
-from django.contrib.gis.geos import Point
 
 
 class FoodSource(models.Model):
     name = models.TextField()
-    address = models.TextField(blank=False, null=True)
-    city = models.TextField(blank=False, null=True)
-    zip = models.IntegerField(blank=False, null=True)
-    main_address = models.TextField(blank=False, null=True)
-    main_city = models.TextField(blank=False, null=True)
-    main_state = models.TextField(blank=False, null=True)
-    main_zip = models.IntegerField(blank=False, null=True)
-    main_phone = models.TextField(blank=False, null=True)
-    main_email = models.EmailField(blank=False, null=True)
-    geo_position = models.PointField(blank=False, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    address = models.TextField(blank=True, null=True)
+    city = models.TextField(blank=True, null=True)
+    zip = models.IntegerField(blank=True, null=True)
+    main_address = models.TextField(blank=True, null=True)
+    main_city = models.TextField(blank=True, null=True)
+    main_state = models.TextField(blank=True, null=True)
+    main_zip = models.IntegerField(blank=True, null=True)
+    main_phone = models.TextField(blank=True, null=True)
+    main_email = models.EmailField(blank=True, null=True)
+    geo_position = models.PointField(blank=True, null=True)
+
+    @property
+    def phone(self):
+        return self.main_phone
+
+    @property
+    def working_hours(self):
+        return [{"start_time": 123, "end_time": 321, "day": "Monday"}]
+
+    @property
+    def latitude(self):
+        if self.geo_position is not None:
+            return self.geo_position.x
+        else:
+            return None
+
+    @property
+    def longitude(self):
+        if self.geo_position is not None:
+            return self.geo_position.y
+        else:
+            return None
--- core_db/src/smile_api/views.py
+++ core_db/src/smile_api/views.py
@@ -1,4 +1,7 @@
+from django.contrib.gis.geos import Point
+
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from smile_api.models import FoodSource
 from smile_api.serializers import FoodSourceSerializer
@@ -7,3 +10,35 @@ from smile_api.serializers import FoodSourceSerializer
 class FoodSourceVeiwSet(viewsets.ReadOnlyModelViewSet):
     queryset = FoodSource.objects.all()
     serializer_class = FoodSourceSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = FoodSource.objects.all()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        data = serializer.data
+
+        if request.GET.get('latitude') is not None and request.GET.get('longitude') is not None:
+            user_position = Point(x=float(request.GET.get('latitude')), y=float(request.GET.get('longitude')))
+
+            sorted_data = {}
+            for address in data:
+                address_position = Point(x=address['latitude'], y=address['longitude'])
+                address['distance'] = user_position.distance(address_position)
+                sorted_data[len(sorted_data)] = address
+
+            for first_pk in sorted_data:
+                for second_pk in sorted_data:
+                    if sorted_data[first_pk]['distance'] < sorted_data[second_pk]['distance']:
+                        buffer = sorted_data[second_pk]
+                        sorted_data[second_pk] = sorted_data[first_pk]
+                        sorted_data[first_pk] = buffer
+
+            data = sorted_data
+
+        response = Response(data)
+        return response
