550d6adf6f4e1a0477123bc228e95a8199d93624
Gaurav Singhal <gaurav281195@gmail.com>
2016-09-25 12:22:46 -0700

    Sms Sender

--- sms_sender/sms_sender.py
+++ sms_sender/sms_sender.py
@@ -19,7 +19,7 @@ class interpret_request:
       message = data['message']
 #      message2 = client.messages.create(to=number, from_=str("+1870498575"),
       message2 = client.messages.create(to=number, messaging_service_sid="MG2bffad310a42593343e6dc64ba226cc1",
-                                        body=message + "Thanks for Contacting. Feel free to ask us anytime.")
+                                        body=message)
 
 
       message2 = client.messages.create(to=number, messaging_service_sid="MG2bffad310a42593343e6dc64ba226cc1",
